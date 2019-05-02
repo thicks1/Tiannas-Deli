63d29cd42e02396e22db48a0cb2d97d77b903de3
thicks1 <49279789+thicks1@users.noreply.github.com>
2019-05-02 09:51:44 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = [];
+var price = [];
+$("button").click(function() {
+    var store = $("#name").val();
+    console.log(name);
+});
+var name = [];
+var price = [];
+$("button").click(function() {
+    var store = $("#price").val();
+    console.log(price);
+    price.push(store);
+});
